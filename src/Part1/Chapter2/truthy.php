048d47b02d44a09289e9a719ee08eaf63bc5a85b
Joseph Edmonds <php-book@php-book.local>
2021-02-22 18:19:07 +0000

    extending truthy to show == and ===

--- src/Part1/Chapter2/truthy.php
+++ src/Part1/Chapter2/truthy.php
@@ -6,7 +6,10 @@ namespace Book\Part1\Chapter2;
 
 function truthy(mixed $input): string
 {
-    $if    = var_export(($input) ? true : false, true);
+    $equal     = var_export($input == true, true);
+    $identical = var_export($input === true, true);
+    $if        = var_export(($input) ? true : false, true);
+
     $empty = var_export(empty($input), true);
     $isset = var_export(isset($input), true);
     switch (true) {
@@ -21,8 +24,9 @@ function truthy(mixed $input): string
         default => 'false',
     };
 
-    return "\nVar: " . var_export($input, true) .
-           "\n If: {$if} | Empty: {$empty} | Isset: {$isset} | Switch: {$switch} | Match: {$match} \n";
+    return "\nVar: " . var_export($input, true)
+           . "\n Equal: {$equal} | Identical: {$identical} | If: {$if} | Empty: {$empty} "
+           . "\n Isset: {$isset} | Switch: {$switch} | Match: {$match} \n";
 }
 
 foreach ([null, 0, 0.0, '0', '', false, true, 1, 'a', 01, -1, 0.1] as $value) {
